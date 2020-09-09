63b73b49b8600c3a6bfbe5ae53940b560ff564a7
carlpadilla <carlpadilla@protonmail.com>
2020-09-08 22:55:51 -0400

    complete project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  // State
   const [inputText, setInputText] = useState('');
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState('all');
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case 'uncompleted':
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
+
+  useEffect(() => {
+    getLocalTodos();
+  }, []);
+
+  useEffect(() => {
+    filterHandler();
+    saveLocalTodos();
+    // eslint-disable-next-line
+  }, [todos, status]);
+
+  // save local
+  const saveLocalTodos = () => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  };
+
+  const getLocalTodos = () => {
+    if (localStorage.getItem('todos') === null) {
+      localStorage.setItem('todos', JSON.stringify([]));
+    } else {
+      let todoLocal = JSON.parse(localStorage.getItem('todos'));
+      setTodos(todoLocal);
+    }
+  };
 
   return (
     <div className='App'>
@@ -18,8 +59,13 @@ function App() {
         inputText={inputText}
         setTodos={setTodos}
         todos={todos}
+        setStatus={setStatus}
       />
-      <TodoList setTodos={setTodos} todos={todos} />
+      <TodoList
+        setTodos={setTodos}
+        todos={todos}
+        filteredTodos={filteredTodos}
+      />
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -20,7 +20,9 @@ const Todo = ({ text, setTodos, todos, todo }) => {
   };
   return (
     <div className='todo'>
-      <li className='todo-item'>{text}</li>
+      <li className={`todo-item ${todo.completed ? 'completed' : ''}`}>
+        {text}
+      </li>
       <button onClick={completeHandler} className='complete-btn'>
         <i className='fas fa-check'></i>{' '}
       </button>
